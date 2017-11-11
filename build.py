34207c9c07e6c86e42e2ea5938e93650fb467fe0
sigmoidal <stathis@npcglib.org>
2017-11-11 04:57:00 +0200

    Improving appveyor builds.

--- build.py
+++ build.py
@@ -1,47 +1,72 @@
-from conan.packager import ConanMultiPackager, os, re
+from conan.packager import ConanMultiPackager
+import os, re, platform
+from distutils.util import strtobool
 
-def get_name_version_from_recipe():
+from collections import defaultdict
+
+def get_value_from_recipe(search_string):
     with open("conanfile.py", "r") as conanfile:
         contents = conanfile.read()
-        name = re.search(r'name\s*=\s*"(\S*)"', contents).groups()[0]
-        version = re.search(r'version\s*=\s*"(\S*)"', contents).groups()[0]
-    return name, version
+        result = re.search(search_string, contents)
+    return result
 
+def get_name_from_recipe():
+    return get_value_from_recipe(r'name\s*=\s*"(\S*)"').groups()[0]
+
+def get_version_from_recipe():
+    return get_value_from_recipe(r'version\s*=\s*"(\S*)"').groups()[0]
 
 def get_default_vars():
     username = os.getenv("CONAN_USERNAME", "bincrafters")
     channel = os.getenv("CONAN_CHANNEL", "testing")
-    _, version = get_name_version_from_recipe()
+    version = get_version_from_recipe()
     return username, channel, version
-
 
 def is_ci_running():
-    return os.getenv("APPVEYOR_REPO_NAME", "") or os.getenv("TRAVIS_REPO_SLUG", "")
-
+    return os.getenv("APPVEYOR_REPO_NAME","") or os.getenv("TRAVIS_REPO_SLUG","")
 
 def get_ci_vars():
-    reponame_a = os.getenv("APPVEYOR_REPO_NAME", "")
-    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH", "")
+    reponame_a = os.getenv("APPVEYOR_REPO_NAME","")
+    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH","")
 
-    reponame_t = os.getenv("TRAVIS_REPO_SLUG", "")
-    repobranch_t = os.getenv("TRAVIS_BRANCH", "")
+    reponame_t = os.getenv("TRAVIS_REPO_SLUG","")
+    repobranch_t = os.getenv("TRAVIS_BRANCH","")
 
     username, _ = reponame_a.split("/") if reponame_a else reponame_t.split("/")
-    username = 'bincrafters'
     channel, version = repobranch_a.split("/") if repobranch_a else repobranch_t.split("/")
     return username, channel, version
-
 
 def get_env_vars():
     return get_ci_vars() if is_ci_running() else get_default_vars()
 
+def get_os():
+    return platform.system().replace("Darwin", "Macos")
+
 
 if __name__ == "__main__":
-    name, _ = get_name_version_from_recipe()
+    conan_build_options = {}
+    conan_build_settings = {}
+
+    name = get_name_from_recipe()
     username, channel, version = get_env_vars()
     reference = "{0}/{1}".format(name, version)
     upload = "https://api.bintray.com/conan/{0}/public-conan".format(username)
 
-    builder = ConanMultiPackager(username=username, channel=channel, reference=reference, upload=upload)
-    builder.add({}, {}, {}, {})
+    os.environ["CONAN_UPLOAD"] = "https://api.bintray.com/conan/{0}/public-conan".format(username)
+    os.environ["CONAN_REMOTES"] = "https://api.bintray.com/conan/conan-community/conan, " \
+                                  "https://api.bintray.com/conan/bincrafters/public-conan"
+
+    reference = "{0}/{1}".format(name, version)
+
+    builder = ConanMultiPackager(args="--build missing",
+                                 username=username,
+                                 channel=channel,
+                                 reference=reference,
+                                 upload=os.environ["CONAN_UPLOAD"],
+                                 remotes=os.environ["CONAN_REMOTES"],
+                                 upload_only_when_stable=True,
+                                 stable_branch_pattern="stable/*")
+
+    builder.add(settings=conan_build_settings, options=conan_build_options)
+
     builder.run()
