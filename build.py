ae0515e11f2fa923972170e56238516713ad31cc
SSE4 <tomskside@gmail.com>
2018-09-22 02:59:36 +0700

    - major recipe cleanup
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -1,11 +1,10 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_default
+from bincrafters import build_template_installer
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_installer.get_builder()
     builder.run()
