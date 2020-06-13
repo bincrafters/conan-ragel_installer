01d4f2b8124801fe1758f6e7a3e18411f4be10d3
Croydon <git@cr0ydon.com>
2020-06-13 19:30:40 +0200

    Apply newer Conan conventions

--- conanfile.py
+++ conanfile.py
@@ -1,19 +1,20 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment, CMake
 import os
+import glob
 
 
 class RagelConan(ConanFile):
     name = "ragel_installer"
-    version = "6.10"
     description = "Ragel compiles executable finite state machines from regular languages"
     homepage = "http://www.colm.net/open-source/ragel"
-    settings = ("os_build", "arch_build", "compiler")
     url = "https://github.com/bincrafters/conan-ragel_installer"
     license = "GPL-2.0"
-    topics = ("conan", "ragel", "installer", "FSM", "regex", "fsm-compiler")
-    exports = "LICENSE.md"
-    exports_sources = ["CMakeLists.txt", "config.h", "0001-unistd.patch"]
+    topics = ("conan", "ragel", "FSM", "regex", "fsm-compiler")
+    exports_sources = ["CMakeLists.txt", "config.h", "patches/*"]
     generators = "cmake"
+
+    settings = "os", "arch", "compiler"
+
     _autotools = None
 
     @property
@@ -21,10 +22,9 @@ class RagelConan(ConanFile):
         return "source_subfolder"
 
     def source(self):
-        sha256 = "5f156edb65d20b856d638dd9ee2dfb43285914d9aa2b6ec779dac0270cd56c3f"
-        source_url = 'http://www.colm.net/files/ragel/ragel-{0}.tar.gz'.format(self.version)
-        tools.get(source_url, sha256=sha256)
-        extracted_dir = "ragel-" + self.version
+        tools.get(**self.conan_data["sources"][self.version])
+        # TODO: extracted_dir = glob.glob(self.name + "-*/")[0]
+        extracted_dir = glob.glob("ragel" + "-*/")[0]
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_autotools(self):
@@ -39,8 +39,10 @@ class RagelConan(ConanFile):
         return cmake
 
     def build(self):
-        if self.settings.os_build == "Windows":
-            tools.patch(self._source_subfolder, "0001-unistd.patch")
+        for patch in self.conan_data["patches"].get(self.version, []):
+            tools.patch(**patch)
+    
+        if self.settings.os == "Windows":
             cmake = self._configure_cmake()
             cmake.build()
         else:
@@ -50,16 +52,18 @@ class RagelConan(ConanFile):
     def package(self):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
         self.copy(pattern="CREDITS", dst="licenses", src=self._source_subfolder)
-        if self.settings.os_build == "Windows":
+        if self.settings.os == "Windows":
             cmake = self._configure_cmake()
             cmake.install()
         else:
             autotools = self._configure_autotools()
             autotools.install()
 
-    def package_info(self):
-        self.env_info.RAGEL_ROOT = self.package_folder
-        self.env_info.PATH.append(os.path.join(self.package_folder, 'bin'))
-
     def package_id(self):
         del self.info.settings.compiler
+
+    def package_info(self):
+        self.env_info.RAGEL_ROOT = self.package_folder
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(bindir))
+        self.env_info.PATH.append(bindir)
